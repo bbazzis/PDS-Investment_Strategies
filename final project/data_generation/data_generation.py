9b6e0a2c1fe24ec703be67a267086bdf7b254509
lesmesrafa <58077392+lesmesrafa@users.noreply.github.com>
2022-04-16 18:56:04 +0200

    Final version

--- final project/data_generation/data_generation.py
+++ final project/data_generation/data_generation.py
@@ -14,37 +14,41 @@ class Portfolio():
     """
     This class allows to create a portfolio and also clean datasets from web scraping part.
     """
-    def __init__(self, path_to_folder: str, assets: str, increment_decrement: float=20.0):
+    def __init__(self, folder_path: str, assets: str, increment_decrement: float=20.0):
         """
         Init Portfolio. Sets the increase/decrease of the portfolio and the assets from which I want to create the portfolio. 
         
         Args:
-            path_to_folder (str): 
+            folder_path (str): 
                 A string indicating the path to the folder where the csv file is to be saved.
             assets (str):
                 A string that contains the acronyms of the different assets from which the portfolio is to be created.
             increment_decrement (float): 
                 A float that represent the increase/decrease of the portfolio. By default = 20.0 (see exercise statement).
-            
         """
         
-        assert increment_decrement > 0 and increment_decrement < 101, "Increment/decrement value must be higher than 1 and less than 101"
-        self.path_to_folder = path_to_folder
-        if os.path.isdir(f"{self.path_to_folder}") == False: os.mkdir(f"{self.path_to_folder}")
+        self.folder_path = folder_path
+        if os.path.isdir(self.folder_path) == False: os.mkdir(self.folder_path)
         self.increment_decrement = increment_decrement 
         available_assets = ["ST", "CB", "PB", "GO", "CA"]
         param_list_assets = list(assets.split(" "))
         unique_assets = list(set([asset for asset in param_list_assets if asset in available_assets]))
         self.assets = [asset for asset in available_assets if asset in unique_assets]
-        assert len(self.assets) >= 1, "The assets selected are not available"
-        
+        # Parameter checking.
+        try:
+            assert increment_decrement > 0 and increment_decrement < 101, "\033[1m ERROR: \033[0m Increment/decrement value must be higher than 1 and less than 101"
+            assert len(self.assets) >= 1, f"\033[1m ERROR: \033[0m At least on of the assets selected ({assets}) are not available."
+        except AssertionError as error:
+            print(error)
+            exit(1)
+            
         
     def generate_portfolio_allocations_csv(self):
         """
-        Create portfolio allocations in the route specified in path_to_folder parameter and returns it as pd.DataFrame object.
+        Create portfolio allocations in the route specified in folder_path parameter and returns it as pd.DataFrame object.
         
         Args:
-            path_to_folder (str): 
+            folder_path (str): 
                 A string indicating the path to the folder where the csv file is to be saved.
         Returns:
             pd.DataFrame object which is the portfolio allocations.
@@ -54,17 +58,17 @@ class Portfolio():
         generate_portfolios = sorted(product(weights, repeat=len(self.assets)), reverse=True)
         portfolio_allocations = pd.DataFrame([portfolio for portfolio in list(generate_portfolios) if sum(portfolio) == 100], columns=self.assets)
         
-        portfolio_allocations.to_csv(path_or_buf=f"{self.path_to_folder}/portfolio_allocations.csv", index=False)
+        portfolio_allocations.to_csv(path_or_buf= self.folder_path + "/portfolio_allocations.csv", index=False)
         
         return portfolio_allocations
         
         
-    def treat_csv_files(self, path_to_folder: str):
+    def treat_csv_files(self, web_scraping_csv_folder_path: str):
         """
-        Treat the csv files from web scraping part stored in path_to_folder route.
+        Treat the csv files from web scraping part stored in folder_path route.
         
         Args:
-            path_to_folder (str): 
+            web_scraping_csv_folder_path (str): 
                 A string indicating the path where are the csv files from web scraping part.
         Returns:
             dict: 
@@ -76,30 +80,37 @@ class Portfolio():
         asset_csv_names = {"ST":"amundi-msci-wrld-ae-c.csv", "CB":"ishares-global-corporate-bond-$.csv", "PB":"db-x-trackers-ii-global-sovereign-5.csv", "GO":"spdr-gold-trust.csv", "CA":"usdollar.csv"}
         remove_keys = [asset for asset in asset_csv_names.keys() if asset not in self.assets]
         for asset in remove_keys: del asset_csv_names[asset] 
-        path_csvs_availables = [f"{path_to_folder}/{csv_name}" for _, csv_name in asset_csv_names.items() if os.path.exists(f"{path_to_folder}/{csv_name}")] 
-        if len(asset_csv_names) != len(path_csvs_availables): exit("Error, the csv files of the desired assets are not available in the specified path (path to folder csv files from webscraping cannot be reached)")
+        path_csvs_availables = [web_scraping_csv_folder_path + "/" + csv_name for _, csv_name in asset_csv_names.items() if os.path.exists(web_scraping_csv_folder_path + "/" + csv_name)] 
+        
+        # Parameter checking.
+        try:
+            assert len(asset_csv_names) == len(path_csvs_availables), "\033[1m ERROR: \033[0m The csv files for all desired assets are not available in the specified path"
+        except AssertionError as error:
+            print(error)
+            exit(1)
+        
         csvs_treated = {}
         type_asset_index = 0
-        # Modify each csv file if it exist
+        # Modify each csv file if it exist.
         for csv_file in path_csvs_availables:
     
             df = pd.read_csv(csv_file)
-            # Date to str
+            # Date to str.
             df["Date"] = pd.to_datetime(df["Date"])
             df["Date"] = df["Date"].dt.date
             df["Date"] = df["Date"].apply(lambda x: str(x))
-            # Col name changed and % character deleted
+            # Col name changed and % character deleted.
             df = df.rename(columns={"Change %": "Change"})
             df["Change"] = pd.to_numeric(df["Change"].apply(lambda x: x.replace("%", "")))
             # Missing dates
             missing_dates = pd.date_range(start='2020-01-01', end='2020-12-31').difference(df["Date"])
             missing_dates_df = pd.DataFrame(missing_dates.strftime('%Y-%m-%d'), columns=["Date"])
-            # For new dates, columns "Price" and "Change" are assigned the average of each column
+            # For new dates, columns "Price" and "Change" are assigned the average of each column.
             avg_price = df["Price"].mean()
             missing_dates_df["Price"] = round(avg_price, 2)
             avg_change = df["Change"].mean()
             missing_dates_df["Change"] = round(avg_change, 2)
-            # If "Vol." column in pd.DataFrame then replace "K" (1e+3), "M" (1e+6) and "-" characters by mean of the column. The missing date rows the same
+            # If "Vol." column in pd.DataFrame then replace "K" (1e+3), "M" (1e+6) and "-" characters by mean of the column. The missing date rows the same.
             if "Vol." in df.columns:
                 df = df.rename(columns={"Vol.": "Vol"})
                 units_val = {"K":1e+3, "M":1e+6}
@@ -125,11 +136,9 @@ class Portfolio():
 
     def generate_portfolio_metrics_csv(self, treat_csv_files: dict, portfolio_allocations: pd.DataFrame, purchase_date: str="2020-01-01"):
         """
-        Create portfolio metrics in path_to_folder route.
+        Create portfolio metrics in folder_path route.
         
         Args:
-            path_to_folder (str): 
-                A string indicating the path to the folder where the csv file is to be saved.
             treat_csv_files (dict):
                 A dictionary in wich the key is the acronym of the asset and the value the treat csv in pd.DataFrame format.
             portfolio_allocations (pd.DataFrame):
@@ -140,8 +149,12 @@ class Portfolio():
 
         # Parameter checking.
         match = re.search("^(2020)(-)(0[1-9]|1[0-2])(-)(0[1-9]|1[0-9]|2[0-9]|3[0-1])$", purchase_date)
-        if match == None: exit("Date format is not correct, the format must be the following: YYYY-MM-DD")
-        if purchase_date in ["2020-02-30", "2020-02-31"]: exit("The introduced date does not exist")
+        try:
+            assert match != None, "\033[1m ERROR: \033[0m The date selected is not available or date format is not correct, the format must be the following: YYYY-MM-DD"
+            assert purchase_date not in ["2020-02-30", "2020-02-31", "2020-04-31", "2020-06-31", "2020-09-31", "2020-11-31"], "\033[1m ERROR: \033[0m The introduced date does not exist."
+        except AssertionError as error:
+            print(error)
+            exit(1)
         
         money_invested = 10000 # This value really does not matter.
         allocations = portfolio_allocations.to_dict("list")
@@ -165,7 +178,7 @@ class Portfolio():
             # Prices for all days since purchase date (included).
             prices = list(df_asset[df_asset["Date"].astype('string') >= purchase_date]["Price"])
             # Calculating sharesji * priceji for all assets for all days for in all portfolios.
-            asset_values[asset] = np.array([np.array([num_shares * price for price in prices]) for num_shares in bj_num_shares]).T # Numpy is faster (implemented in C++)
+            asset_values[asset] = np.array([np.array([num_shares*price for price in prices]) for num_shares in bj_num_shares]).T # Numpy is faster (implemented in C++)
 
         # Valuei final calculation, (sum of matrix asset_values[asset]).
         days_portfolio_prices = 0
@@ -182,4 +195,4 @@ class Portfolio():
         portfolio_allocations["RETURN"] = round(((current_value-buy_amount)/buy_amount)*100, 3)
         portfolio_allocations["VOLAT"] = round((std_dev/sample_avg)*100, 3)   
         
-        portfolio_allocations.to_csv(path_or_buf=f"{self.path_to_folder}/portfolio_metrics.csv", index=0)           
+        portfolio_allocations.to_csv(path_or_buf= self.folder_path + "/portfolio_metrics.csv", index=0)           
--- final project/web_scraping/web_scraping.py
+++ final project/web_scraping/web_scraping.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
 This file contains all the code implementation corresponding to the part 1 of the project.
-The code below, through webscraping on the web page https://www.investing.com/ obtains 5 datasets corresponding to 5 desired assets.
-The datasets are generated in the directory 'csv_files'. 
+The code below, through webscraping on the web page https://www.investing.com/ obtains 5 datasets corresponding to 5 desired assets (depending on the parameters introduced by the user).
+
 """
 
 from selenium import webdriver
@@ -29,29 +29,26 @@ class ObtainCSVFilesFromWeb():
                 A string represents the URL to the web page.
         """
         
-        # Checking parameter values
-        assert isinstance(web_browser, str), "web_browser variable must be string type" 
-        assert isinstance(web_page, str), "web_page variable must be string type"
-        assert web_browser == "Chrome", f"web_browser variable must be 'Chrome', {web_browser} is not allowed"
-
         self.web_page = web_page
         if web_browser == "Chrome":
             chrome_options = Options()
             chrome_options.add_argument("--start-maximized")
-            # chrome_options.add_argument("--headless")
-            # chrome_options.add_argument("--no-sandbox")
-            # chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--log-level=1")
+            chrome_options.add_argument("--disable-extensions")
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--window-size=1920x1080")
             self.driver = webdriver.Chrome(options=chrome_options)
     
     
-    def create_datasets_from_investing(self, path_to_folder: str, dataset: str):
+    def create_datasets_from_investing(self, folder_path: str, dataset_name: str):
         """
-        Using webscraping, this function create a csv file(s) in "path_to_folder" folder.
+        Using webscraping, this function create a csv file(s) in "folder_path" folder.
         
         Args:
-            path_to_folder (str):
+            folder_path (str):
                 A string indicating the path to the folder where the csv file(s) is to be saved.
-            dataset (str): 
+            dataset_name (str): 
                 A string representing the csv file to be created. It can take one of the following values: 
                  - "All" to create all datasets.
                  - "Stocks" to create amundi-msci-wrld-ae-c.csv
@@ -75,61 +72,62 @@ class ObtainCSVFilesFromWeb():
             "US78463V1070":"spdr-gold-trust.csv", 
             "US Dollar Index (DXY)":"usdollar.csv"
         }
-        assert self.web_page == "https://www.investing.com/", f"web_page must be 'https://www.investing.com/', {self.web_page} is not valid for this function"
+        datasets_available = {"Stocks":["CRPS", "XG7S", "US78463V1070", "US Dollar Index (DXY)"], 
+                              "Corporate bonds":["0P00012PP6", "US Dollar Index (DXY)", "XG7S", "US78463V1070"],
+                              "Public bonds":["0P00012PP6", "CRPS", "US78463V1070", "US Dollar Index (DXY)"],
+                              "Golds":["0P00012PP6", "CRPS", "XG7S", "US Dollar Index (DXY)"], 
+                              "Cash":["0P00012PP6", "CRPS", "XG7S", "US78463V1070"]
+        }   
+        # Parameter checking.
+        try:
+            assert dataset_name == "All" or dataset_name in datasets_available.keys(), f"\033[1m ERROR: \033[0m Please, select a valid .csv file, '{dataset_name}' not available."
+        except AssertionError as error:
+            print(error)
+            exit(1)
         
-        datasets_available = ["All", "Stocks", "Corporate bonds", "Public bonds", "Golds", "Cash"]
-        
-        if dataset not in datasets_available: exit(f"Please, select a valid dataset, '{dataset}' not available") 
-            
-        if dataset == datasets_available[1]:
-            del assetsid_stockmarket["CRPS"], assetsid_stockmarket["XG7S"], assetsid_stockmarket["US78463V1070"], assetsid_stockmarket["US Dollar Index (DXY)"]
-        elif dataset == datasets_available[2]:
-            del assetsid_stockmarket["0P00012PP6"], assetsid_stockmarket["XG7S"], assetsid_stockmarket["US78463V1070"], assetsid_stockmarket["US Dollar Index (DXY)"]
-        elif dataset == datasets_available[3]:
-            del assetsid_stockmarket["0P00012PP6"], assetsid_stockmarket["CRPS"], assetsid_stockmarket["US78463V1070"], assetsid_stockmarket["US Dollar Index (DXY)"]
-        elif dataset == datasets_available[4]:
-            del assetsid_stockmarket["0P00012PP6"], assetsid_stockmarket["CRPS"], assetsid_stockmarket["XG7S"], assetsid_stockmarket["US Dollar Index (DXY)"]
-        elif dataset == datasets_available[5]:
-            del assetsid_stockmarket["0P00012PP6"], assetsid_stockmarket["CRPS"], assetsid_stockmarket["XG7S"], assetsid_stockmarket["US78463V1070"]
-        
+        if dataset_name in datasets_available.keys():
+            datasets_available = datasets_available.pop(dataset_name)
+            for elem in datasets_available:
+                del assetsid_stockmarket[elem]
+                
         driver = self.driver
         driver.get(self.web_page)
         try:
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]'))).click() # Closing popup
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]'))).click() # Closing popup.
 
             for asset_code, stockmarket in assetsid_stockmarket.items():
-                # Write the asset code in the searcher line 
+                # Write the asset code in the searcher line.
                 driver.find_element(by=By.TAG_NAME, value="INPUT").send_keys(asset_code) 
-                # Search button
+                # Search button.
                 driver.find_element(by=By.TAG_NAME, value="LABEL").click() 
-                # Select desired asset 
+                # Select desired asset.
                 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, stockmarket))).click() 
-                # Historical data button
+                # Historical data button.
                 driver.find_element(by=By.LINK_TEXT, value="Historical Data").click() 
-                # Opening calendar
+                # Opening calendar.
                 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "widgetFieldDateRange"))).click() 
-                # Modify startDate value
+                # Modify startDate value.
                 start_date_search_bar = driver.find_element(by=By.ID, value="startDate") 
                 start_date_search_bar.clear()
                 start_date_search_bar.send_keys("01/01/2020") 
-                # Modify endDate value
+                # Modify endDate value.
                 end_date_search_bar = driver.find_element(by=By.ID, value="endDate")
                 end_date_search_bar.clear()
                 end_date_search_bar.send_keys("12/31/2020")
-                # Applying date changes to the calendar
+                # Applying date changes to the calendar.
                 driver.find_element(by=By.ID, value="applyBtn").click()
-                # Wait until the table is available
+                # Wait until the table is available.
                 html_table = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "curr_table"))) 
-                # Table to html form
-                df_html = pd.read_html(html_table.get_attribute('outerHTML'), index_col=0) 
-                # Table html to pandas DataFrame
+                # Table to html form.
+                df_html = pd.read_html(html_table.get_attribute("outerHTML"), index_col=0) 
+                # Table html to pandas DataFrame.
                 df = pd.DataFrame(df_html[0]).drop(["Open", "High", "Low"], axis=1).convert_dtypes() 
-                # For US Dolar, compute the value of 1 US dolar for that day
+                # For US Dolar, compute the value of 1 US dolar for that day.
                 if asset_code == "US Dollar Index (DXY)": df["Price"] = round(pd.to_numeric(df["Price"])/100, 3)
                 
-                if os.path.isdir(f"{path_to_folder}") == False: os.mkdir(f"{path_to_folder}") 
+                if os.path.isdir(folder_path) == False: os.mkdir(folder_path) 
                    
-                df.to_csv(path_or_buf=f"{path_to_folder}/{assetsid_csvname[asset_code]}") 
+                df.to_csv(path_or_buf= folder_path + "/" + assetsid_csvname[asset_code]) 
 
         finally :
             driver.close() 
